e8b1d63dda2f8fe57ecea66f6a38e042408c27b6
willty412 <itssinq@gmail.com>
2022-05-01 00:18:53 -0400

    oops2

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const Discord = require("discord.js")
 require("dotenv").config
 
-/*
-const TOKEN = "OTcwMTY5MzExNjQ3ODk1NjI0.Ym4ClA.cm3MSg_qKvlWjn3UuNLD59rW--A"
-*/
-
 const client = new Discord.Client({
     intents: [
         "GUILDS",
